bc3e6f3fda3cbc79d2f9ce8a18dee9d1c217a1b4
Juliana Meneguete <jumeneguete@gmail.com>
2021-09-23 19:41:26 -0300

    feat: configs of tsconfig.json

--- src/middlewares/errorMiddleware.ts
+++ src/middlewares/errorMiddleware.ts
@@ -2,7 +2,6 @@ import { Request, Response, NextFunction } from "express";
 import httpStatus from "http-status";
 
 import ConflictError from "../errors/ConflictError";
-import EmailAlreadyRegistered from "../errors/EmailAlreadyRegistered";
 
 export default function errorMiddleware(
   err: Error,
@@ -10,6 +9,7 @@ export default function errorMiddleware(
   res: Response,
   _next: NextFunction
 ) {
+
   if (err instanceof ConflictError) {
     return res.status(httpStatus.CONFLICT).send({
       message: err.message,
