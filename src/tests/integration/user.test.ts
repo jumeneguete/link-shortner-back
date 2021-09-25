1bcbaa2e8a48c3ce21ce73242e30c07deac35ffa
Juliana Meneguete <jumeneguete@gmail.com>
2021-09-25 13:46:17 -0300

    test: post /sign-up to create a new user

--- src/tests/integration/user.test.ts
+++ src/tests/integration/user.test.ts
@@ -1,4 +1,6 @@
-import supertest from "supertest";
+import supertest from 'supertest'
+import httpStatus from "http-status";
+import faker from "faker";
 
 import app, { init } from "../../app";
 import { clearDatabase, endConnection } from "../utils/database";
@@ -18,12 +20,30 @@ afterAll(async () => {
   await endConnection();
 });
 
-describe("POST /users", () => {
+describe("POST /sign-up", () => {
   it("should create a new user", async () => {
+    const userData = {
+      name: faker.name.findName(),
+      image: faker.internet.avatar(),
+      email: faker.internet.email(),
+      password: "123456"
+    };
 
+    const response = await agent.post("/sign-up").send(userData);
+
+    expect(response.statusCode).toEqual(httpStatus.CREATED);
+    expect(response.body).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        name: expect.any(String),
+        image: expect.any(String),
+        email: expect.any(String),
+        password: expect.any(String)
+      })
+    );
   });
 
-  it("should not allow creation of user with email that has been already used", async () => {
-
-  });
+  // it("should not allow creation of user with email that has been already used", async () => {
+  
+  // });
 });
