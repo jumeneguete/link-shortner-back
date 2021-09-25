6c0334dc6c6efd1a3803e74a4177320a6e3fd39e
Juliana Meneguete <jumeneguete@gmail.com>
2021-09-25 15:39:04 -0300

    fix: eslint warning

--- src/migrations/1632264903956-CreateTablesUsersSessionsLinks.ts
+++ src/migrations/1632264903956-CreateTablesUsersSessionsLinks.ts
@@ -4,18 +4,18 @@ export class CreateTablesUsersSessionsLinks1632264903956 implements MigrationInt
     name = 'CreateTablesUsersSessionsLinks1632264903956'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-    	await queryRunner.query('CREATE TABLE "users" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "image" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))');
-    	await queryRunner.query('CREATE TABLE "links" ("id" SERIAL NOT NULL, "originUrl" character varying NOT NULL, "code" character varying NOT NULL, "accessCounter" character varying NOT NULL, "userId" integer NOT NULL, CONSTRAINT "PK_ecf17f4a741d3c5ba0b4c5ab4b6" PRIMARY KEY ("id"))');
-    	await queryRunner.query('CREATE TABLE "sessions" ("id" SERIAL NOT NULL, "token" character varying NOT NULL, "userId" integer NOT NULL, CONSTRAINT "PK_3238ef96f18b355b671619111bc" PRIMARY KEY ("id"))');
-    	await queryRunner.query('ALTER TABLE "links" ADD CONSTRAINT "FK_56668229b541edc1d0e291b4c3b" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION');
-    	await queryRunner.query('ALTER TABLE "sessions" ADD CONSTRAINT "FK_57de40bc620f456c7311aa3a1e6" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION');
+      await queryRunner.query('CREATE TABLE "users" ("id" SERIAL NOT NULL, "name" character varying NOT NULL, "image" character varying NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))');
+      await queryRunner.query('CREATE TABLE "links" ("id" SERIAL NOT NULL, "originUrl" character varying NOT NULL, "code" character varying NOT NULL, "accessCounter" character varying NOT NULL, "userId" integer NOT NULL, CONSTRAINT "PK_ecf17f4a741d3c5ba0b4c5ab4b6" PRIMARY KEY ("id"))');
+      await queryRunner.query('CREATE TABLE "sessions" ("id" SERIAL NOT NULL, "token" character varying NOT NULL, "userId" integer NOT NULL, CONSTRAINT "PK_3238ef96f18b355b671619111bc" PRIMARY KEY ("id"))');
+      await queryRunner.query('ALTER TABLE "links" ADD CONSTRAINT "FK_56668229b541edc1d0e291b4c3b" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION');
+      await queryRunner.query('ALTER TABLE "sessions" ADD CONSTRAINT "FK_57de40bc620f456c7311aa3a1e6" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION');
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-    	await queryRunner.query('ALTER TABLE "sessions" DROP CONSTRAINT "FK_57de40bc620f456c7311aa3a1e6"');
-    	await queryRunner.query('ALTER TABLE "links" DROP CONSTRAINT "FK_56668229b541edc1d0e291b4c3b"');
-    	await queryRunner.query('DROP TABLE "sessions"');
-    	await queryRunner.query('DROP TABLE "links"');
-    	await queryRunner.query('DROP TABLE "users"');
+      await queryRunner.query('ALTER TABLE "sessions" DROP CONSTRAINT "FK_57de40bc620f456c7311aa3a1e6"');
+      await queryRunner.query('ALTER TABLE "links" DROP CONSTRAINT "FK_56668229b541edc1d0e291b4c3b"');
+      await queryRunner.query('DROP TABLE "sessions"');
+      await queryRunner.query('DROP TABLE "links"');
+      await queryRunner.query('DROP TABLE "users"');
     }
 }
